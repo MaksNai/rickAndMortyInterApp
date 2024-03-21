dad3cca6ce73ede5f03f980a778bae877c722d7a
Maxim <naidenov.fortech@gmail.com>
2024-03-21 09:30:42 +0300

    fixed filter name

--- src/components/FilterInput/FilterInput.jsx
+++ src/components/FilterInput/FilterInput.jsx
@@ -1,19 +1,20 @@
 import SearchIcon from "@mui/icons-material/Search";
-import { InputBase } from "@mui/material";
-import styles from "./filterInput.module.scss";
+import { FormControl, InputAdornment, TextField } from "@mui/material";
 
 export function FilterInput() {
   return (
-    <label className={styles.label}>
-      <SearchIcon className={styles.icon} />
-      <InputBase
-        id="filterNameCharacter"
-        placeholder="Filter by name or episode (ex. S01 or S01E02)"
-        className={styles.input}
-        sx={{
-            width: "100%",
+    <FormControl>
+      <TextField
+        placeholder="Filter by name..."
+        variant="outlined"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
         }}
-      ></InputBase>
-    </label>
+      />
+    </FormControl>
   );
 }
